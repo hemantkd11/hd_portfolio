4c0a4da9ba1b6566c78e0e8bcf7dcd22c5e0ae6a
Coder-SlayerPro <verma.saurav02262@gmail.com>
2024-07-22 22:13:36 +0530

    new change

--- src/Components/Home/LandingPage.js
+++ src/Components/Home/LandingPage.js
@@ -82,9 +82,43 @@ const Landing_Page = () => {
                 <div className="Landing-page-mob-wrapper">
                   <img src={HD} alt="" />
                   <div className="Landing-page-mob-text">
-                    <h3>HEY THERE !</h3>
-                    <h1>I Am Hemant Diwakar</h1>
-                    <h2>Front-End RaectJS Developer</h2>
+                    <div className="B">
+                      <h3>HEY THERE !</h3>
+                      <h1>I Am Hemant Diwakar</h1>
+                      <h2>Front-End RaectJS Developer</h2>
+                    </div>
+                    <div className="Landing-btn-box">
+                      <div>
+                        <a
+                          href={Resume}
+                          download="HemantDiwakar.pdf"
+                          class="button"
+                        >
+                          <span class="button__text">Download</span>
+                          <span class="button__icon">
+                            <svg
+                              xmlns="http://www.w3.org/2000/svg"
+                              viewBox="0 0 35 35"
+                              id="bdd05811-e15d-428c-bb53-8661459f9307"
+                              data-name="Layer 2"
+                              class="svg"
+                            >
+                              <path d="M17.5,22.131a1.249,1.249,0,0,1-1.25-1.25V2.187a1.25,1.25,0,0,1,2.5,0V20.881A1.25,1.25,0,0,1,17.5,22.131Z"></path>
+                              <path d="M17.5,22.693a3.189,3.189,0,0,1-2.262-.936L8.487,15.006a1.249,1.249,0,0,1,1.767-1.767l6.751,6.751a.7.7,0,0,0,.99,0l6.751-6.751a1.25,1.25,0,0,1,1.768,1.767l-6.752,6.751A3.191,3.191,0,0,1,17.5,22.693Z"></path>
+                              <path d="M31.436,34.063H3.564A3.318,3.318,0,0,1,.25,30.749V22.011a1.25,1.25,0,0,1,2.5,0v8.738a.815.815,0,0,0,.814.814H31.436a.815.815,0,0,0,.814-.814V22.011a1.25,1.25,0,1,1,2.5,0v8.738A3.318,3.318,0,0,1,31.436,34.063Z"></path>
+                            </svg>
+                          </span>
+                        </a>
+                      </div>
+                      <div>
+                        <button
+                          //   onclick="openTab(event,'Get-In-Touch')"
+                          class="tablinks Get-in-touch"
+                        >
+                          Get In Touch
+                        </button>
+                      </div>
+                    </div>
                   </div>
                 </div>
               </div>
