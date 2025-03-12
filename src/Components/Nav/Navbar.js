bc2a71f0079394a9eb153adb3e16fb126458b51b
Coder-SlayerPro <hemantdiwakar15@gmail.com>
2025-03-12 10:16:20 +0530

    new buttons

--- src/Components/Nav/Navbar.js
+++ src/Components/Nav/Navbar.js
@@ -54,18 +54,19 @@ const Navbar = () => {
               <img src={Git} alt="LinkedIn" className="Image_linkedIn" />
             </Link>
           </div>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarTogglerDemo02"
+            aria-controls="navbarTogglerDemo02"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
         </div>
-        <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarTogglerDemo02"
-          aria-controls="navbarTogglerDemo02"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
+
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
           <ul
             class="navbar-nav me-auto mb-2 mb-lg-0"
